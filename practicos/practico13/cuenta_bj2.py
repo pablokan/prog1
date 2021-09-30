915272edd5f8f5c3e7283001964fd15f192316b2
Pablo Kaniefsky <pablokan@gmail.com>
2021-09-30 17:39:46 -0300

    bj11

---
+++ practicos/practico13/cuenta_bj2.py
@@ -0,0 +1,9 @@
+mano = [2, 1, 3, 1, 3, 10]
+
+
+def totales(mano):
+    total = sum(mano)
+    return total, total + 10 if mano.count(1) != 0 and total + 10 < 22 else total
+
+
+print(totales(mano))
