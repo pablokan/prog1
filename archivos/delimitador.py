c6188d07614a388282e39ede5169d362e3168071
Pablo Kaniefsky <pablokan@gmail.com>
2021-11-10 17:34:27 -0300

    delim 2

--- archivos/delimitador.py
+++ archivos/delimitador.py
@@ -3,6 +3,8 @@ import csv
 archivo = open("datitos.txt", "r")
 filas = csv.reader(archivo, delimiter="%")
 
+print(f"{filas=}")
+print("Encabezados", next(filas))
 for fila in filas:
     print(fila)
 
