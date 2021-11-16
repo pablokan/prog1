2089de8e955254ef87735bfc4a636b9633f4ca1d
Pablo Kaniefsky <pablokan@gmail.com>
2021-11-16 13:55:03 -0300

    prueba temas

--- vs/prueba_temas.py
+++ vs/prueba_temas.py
@@ -3,21 +3,29 @@ def foo(par):
         pass
     else:
         return 1
+
+
 # comment
-n, s = 91, 'hola'
-li = [3, 'chau', True, None, (2, 3), int]
-di = {'name': 'John'}
+n, s = 91, "hola"
+li = [3, "chau", True, None, (2, 3), int]
+di = {"name": "John"}
 f = foo(n)
 
 from dataclasses import dataclass
+
+
 @dataclass
-class Persona():
+class Persona:
     name: str
     age: int
+
     def saludo(self):
-        print(f'Hola {self.name}, tenés {self.age} años')
+        print(f"Hola {self.name}, tenés {self.age} años")
 
-p = Persona('Paul', 56)
+    def __str__(self) -> str:
+        return f"{self.name} tiene {self.age} años"
+
+
+p = Persona("Paul", 56)
 p.saludo()
-
-
+print(p)
